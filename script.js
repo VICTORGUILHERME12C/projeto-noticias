0c4315e42277e21b975f3b89e47013e1b4935a48
VICTORGUILHERME12C <164795573+VICTORGUILHERME12C@users.noreply.github.com>
2025-06-01 23:07:34 -0300

    correção de problemas

--- script.js
+++ script.js
@@ -4,17 +4,15 @@ const botaoFechar = document.querySelector("header nav button")
 const body = document.querySelector("body")
 const nav = document.querySelector("header .mobile")
 
-botaoAbrir.addEventListener('click', abrirMenu )
-botaoFechar.addEventListener('click', fecharMenu )
+botaoAbrir.addEventListener('click', abrirMenu)
+botaoFechar.addEventListener('click', fecharMenu)
 
-function abrirMenu(){
+function abrirMenu() {
     body.classList.add("escurecer")
     nav.classList.add("abrir")
-
 }
 
-
-function fecharMenu(){
+function fecharMenu() {
     body.classList.remove("escurecer")
     nav.classList.remove("abrir")
 }
